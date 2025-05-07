21a330f59f1246ea4e0bd81d1c1dc0ac69308e89
Jami Nurmi <jami.nurmi2@gmail.com>
2025-05-08 00:21:25 +0300

    found the bug

--- vite.config.js
+++ vite.config.js
@@ -8,13 +8,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   // Configure React plugin
+  base: '/personal-trainer-app/', // tärkeä osa GitHub Pages -julkaisua varten
   plugins: [react()],
-  base: '/personal-trainer-app/',
 
-  // Base URL path
-  // In production, sets the base to GitHub Pages subpath
-  // In development, uses root path (/)
-  base: process.env.NODE_ENV === 'production'
-    ? '/personalTrainerApp/' // GitHub Pages path - must match repo name
-    : '/',
 });
